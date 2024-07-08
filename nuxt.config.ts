b700cd55613efb3621f5c5712e5d6faf31347302
Dainius258 <dainiuss258@gmail.com>
2024-07-08 14:49:23 +0300

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  i18n: {
+    lazy: true,
+    langDir: "locales",
+    strategy: "prefix",
+    locales: [
+      {
+        code: "en-US",
+        name: "English",
+        iso: "en-US",
+        file: "en.json"
+      },
+      {
+        code: "lt-LT",
+        name: "Lietuvių",
+        iso: "lt-LT",
+        file: "lt.json"
+      },
+    ],
+    defaultLocale: "en-US",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+      redirectOn: "root",
+    },
+  },
+})
---
+++ pages/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="mt-40">Index page</h1>
+    <p>{{ $t('test') }}</p>
+  </div>
+</template>
+
+<script setup>
+const { locale } = useI18n()
+</script>
+
