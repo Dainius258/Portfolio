81e2ea3011be4d3317b307d213c72975a8f7093f
Dainius258 <dainiuss258@gmail.com>
2024-07-23 10:06:10 +0300

    Made a better transition for colorMode component icon

--- components/nav/colorMode.vue
+++ components/nav/colorMode.vue
@@ -1,25 +1,43 @@
 <template>
     <button @click="switchColorMode">
-        <Icon 
-        class="mt-1 size-10 transition-colors duration-200 ease-in-out" 
-        :name="selectedIcon" 
-        :class="{'text-cyan-100 hover:text-cyan-300': colorMode === 'dark','text-yellow-100 hover:text-yellow-300': colorMode === 'light'}"
-        />
+        <transition name="fade" mode="out-in">
+            <Icon 
+            v-if="colorMode === 'light'"
+            class="mt-1 size-10 text-yellow-100 hover:text-yellow-300 transition-colors duration-200 ease-in-out" 
+            name="mdi:lightbulb-on-90" 
+            key="light"
+            />
+            <Icon 
+            v-else
+            class="mt-1 size-10 text-blue-300 hover:text-blue-600 transition-colors duration-200 ease-in-out" 
+            name="mdi:weather-night" 
+            key="dark"
+            />
+        </transition>
     </button>
 </template>
 
 <script setup>
+// mdi:lightbulb-on-90 mdi:weather-night
 const selectedIcon = ref('mdi:lightbulb-on-90')
 const colorMode = ref('light')
 
 const switchColorMode = () => {
-    if (colorMode.value === 'light') {
-        colorMode.value = 'dark'
-        selectedIcon.value = 'mdi:weather-night'
-    } else {
-        colorMode.value = 'light'
-        selectedIcon.value = 'mdi:lightbulb-on-90'
-    }
+  colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'
 }
-// mdi:lightbulb-on-90 mdi:weather-night
 </script>
+
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.1s;
+}
+
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave-from {
+  opacity: 1;
+}
+</style>
