440041efaa52c2d2ea499df3dffd96ee6581169d
Dainius258 <dainiuss258@gmail.com>
2024-07-25 18:41:19 +0300

    Added slide in animations for timeline

--- components/nav/index.vue
+++ components/nav/index.vue
@@ -2,8 +2,8 @@
     <div class="bg-secondary size-fit flex flex-row rounded-full justify-between p-1 border-4 shadow-lg border-primary">
         <div class="flex flex-row justify-center">
             <NavButton @click="emitScrollEvent('about')"  :text="$t('nav.about')"/>
-            <NavButton @click="emitScrollEvent('experience')" :text="$t('nav.expierence')"/>
             <NavButton @click="emitScrollEvent('projects')" :text="$t('nav.projects')"/>
+            <NavButton @click="emitScrollEvent('experience')" :text="$t('nav.expierence')"/>
             <NavButton @click="emitScrollEvent('contact')" :text="$t('nav.contact')"/>
         </div>
         <div class="flex flex-row ml-4">
--- components/timelineExpierence/index.vue
+++ components/timelineExpierence/index.vue
@@ -5,15 +5,30 @@
       <div class="h-16"></div>
       <div class="space-y-8">
         <div v-for="(item, index) in experience" :key="index" class="flex items-start relative">
-         <div class="w-1/2" :class="{ 'order-1 mr-20  text-right': index % 2 === 0, 'order-2 ml-20': index % 2 !== 0 }">
+         <div 
+         v-element-visibility="state => onCardVisibility(state, index)" 
+         class="w-1/2" 
+         :class="{ 
+          'order-1 mr-20  text-right slide-in-right': index % 2 === 0 && cardVisible[index], 
+          'order-2 ml-20 slide-in-left': index % 2 !== 0 && cardVisible[index],
+          'opacity-0 translate-x-100': !cardVisible[index],
+          }">
             <ItemCard :title="item.title" :subtitle="item.location" :description="item.description"/>
           </div>
           <!-- Checkpoint -->
-          <div class="absolute left-1/2 transform -translate-x-1/2 w-16 h-16 rounded-full bg-white flex items-center justify-center">
-            <NuxtImg :src="item.img" />
+          <div 
+          class="absolute left-1/2 transform -translate-x-1/2 w-16 h-16 rounded-full bg-white flex items-center justify-center"
+          >
+            <NuxtImg class=" size-full p-3" :src="item.img" />
           </div>
           <!-- Date -->
-          <div class="w-1/2 mt-5" :class="{ 'order-2': index % 2 === 0, 'order-1 text-right': index % 2 !== 0 }">
+          <div 
+          class="w-1/2 mt-5" 
+          :class="{ 
+            'order-2 appear': index % 2 === 0 && cardVisible[index],  
+            'order-1 text-right appear': index % 2 !== 0 && cardVisible[index],
+            'opacity-0 translate-x-100': !cardVisible[index],
+            }">
           <p>{{item.date}}</p>
           </div>
         </div>
@@ -24,6 +39,14 @@
 </template>
 
 <script setup>
+import { vElementVisibility } from '@vueuse/components'
+
+const cardVisible = ref([])
+function onCardVisibility(state, index) {
+  if (state) {
+    cardVisible.value[index] = true
+  }
+}
 
 const props = defineProps({
     experience: {
@@ -31,6 +54,46 @@ const props = defineProps({
         required: true
     }
 })
-
-
 </script>
+
+<style scoped>
+@keyframes slide-in-right {
+  0% {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-left {
+  0% {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.appear {
+  animation: appear 1s ease-out forwards;
+}
+.slide-in-right {
+  animation: slide-in-right 1s ease-out forwards;
+}
+.slide-in-left {
+  animation: slide-in-left 1s ease-out forwards;
+}
+
+</style>
